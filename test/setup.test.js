60ec5c48d6282d65263373f1912e5e5d972a25ad
Anna Lopez <annalopezribo@gmail.com>
2022-04-12 16:19:36 +0200

    before ach and after enabled again

--- test/setup.test.js
+++ test/setup.test.js
@@ -5,15 +5,15 @@ const Artist = require("../models/artist");
 const User = require("../models/user");
 
 //clean up the database before and after each test
-// // before((done) => {
-// beforeEach((done) => {
-//   Artist.deleteMany({}, function (err) {});
-//   User.deleteMany({}, function (err) {});
-//   done();
-// });
-// // after((done) => {
-// afterEach((done) => {
-//   Artist.deleteMany({}, function (err) {});
-//   User.deleteMany({}, function (err) {});
-//   done();
-// });
+// before((done) => {
+beforeEach((done) => {
+  Artist.deleteMany({}, function (err) {});
+  User.deleteMany({}, function (err) {});
+  done();
+});
+// after((done) => {
+afterEach((done) => {
+  Artist.deleteMany({}, function (err) {});
+  User.deleteMany({}, function (err) {});
+  done();
+});
